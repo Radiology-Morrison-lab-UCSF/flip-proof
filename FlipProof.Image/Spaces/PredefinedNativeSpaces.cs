1c6f09263c939e7c84e30acb7d4ca8b98e8b3d38
Lee Reid <lee.reid@fastmail.com>
2025-03-01 23:00:17 -0800

    added some premade spaces

--- FlipProof.Image/Spaces/PredefinedNativeSpaces.cs
+++ FlipProof.Image/Spaces/PredefinedNativeSpaces.cs
@@ -1,22 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FlipProof.Image.Spaces;
-public struct NativePD : ISpace
+﻿namespace FlipProof.Image.Spaces
 {
+   public struct NativePD : ISpace { }
+   public struct NativeT1 : ISpace { }
+   public struct NativeT2 : ISpace { }
+   public struct NativeDWI : ISpace { }
+   public struct NativeDTI : ISpace { }
+   public struct NativeFMRI : ISpace { }
+   public struct NativeQSM : ISpace { }
+   public struct NativeMagnitude : ISpace { }
+   public struct NativePhase : ISpace { }
+   public struct NativeCT : ISpace { }
+   public struct NativePET : ISpace { }
+
+   public struct MNI152 : ISpace { }
+   public struct MNI305 : ISpace { }
+   public struct Talairach : ISpace { }
 }
 
-public struct NativeT1 : ISpace
+namespace FlipProof.Image.Spaces.Subject1
 {
+   public struct NativePD : ISpace { }
+   public struct NativeT1 : ISpace { }
+   public struct NativeT2 : ISpace { }
+   public struct NativeDWI : ISpace { }
+   public struct NativeDTI : ISpace { }
+   public struct NativeFMRI : ISpace { }
+   public struct NativeQSM : ISpace { }
+   public struct NativeMagnitude : ISpace { }
+   public struct NativePhase : ISpace { }
+   public struct NativeCT : ISpace { }
+   public struct NativePET : ISpace { }
+
 }
 
-public struct NativeT2 : ISpace
+namespace FlipProof.Image.Spaces.Subject2
 {
+   public struct NativePD : ISpace { }
+   public struct NativeT1 : ISpace { }
+   public struct NativeT2 : ISpace { }
+   public struct NativeDWI : ISpace { }
+   public struct NativeDTI : ISpace { }
+   public struct NativeFMRI : ISpace { }
+   public struct NativeQSM : ISpace { }
+   public struct NativeMagnitude : ISpace { }
+   public struct NativePhase : ISpace { }
+   public struct NativeCT : ISpace { }
+   public struct NativePET : ISpace { }
+
 }
 
-public struct MNI152 : ISpace
+namespace FlipProof.Image.Spaces.Subject3
 {
+   public struct NativePD : ISpace { }
+   public struct NativeT1 : ISpace { }
+   public struct NativeT2 : ISpace { }
+   public struct NativeDWI : ISpace { }
+   public struct NativeDTI : ISpace { }
+   public struct NativeFMRI : ISpace { }
+   public struct NativeQSM : ISpace { }
+   public struct NativeMagnitude : ISpace { }
+   public struct NativePhase : ISpace { }
+   public struct NativeCT : ISpace { }
+   public struct NativePET : ISpace { }
+
+}
+
+
+namespace FlipProof.Image.Spaces.Subject4
+{
+   public struct NativePD : ISpace { }
+   public struct NativeT1 : ISpace { }
+   public struct NativeT2 : ISpace { }
+   public struct NativeDWI : ISpace { }
+   public struct NativeDTI : ISpace { }
+   public struct NativeFMRI : ISpace { }
+   public struct NativeQSM : ISpace { }
+   public struct NativeMagnitude : ISpace { }
+   public struct NativePhase : ISpace { }
+   public struct NativeCT : ISpace { }
+   public struct NativePET : ISpace { }
+
 }
